62bdee37317bad220e04d6e96bc784e560f86c4f
Unknown <nannyakannya@gmail.com>
2019-05-02 18:17:15 +0900

    improve webview

--- rujaion/webview_widget.py
+++ rujaion/webview_widget.py
@@ -1,46 +1,49 @@
 import sys
-from PyQt5.QtWidgets import QWidget, QGridLayout, QApplication
-from PyQt5 import QtGui, QtCore
+from PyQt5 import QtWidgets
+from PyQt5 import QtGui
 from PyQt5.QtCore import QUrl
 from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+# Many of source is copied from https://qiita.com/montblanc18/items/88d0b639de86b7cac613
 
 
 class CustomWebEngineView(QWebEngineView):
     def contextMenuEvent(self, a0: QtGui.QContextMenuEvent) -> None:
-        menu = self.page().createStandardContextMenu()
-        menu.addSeparator()
+        menu = QtWidgets.QMenu()
         menu.addAction(u"Download This Task", self.parent().download_task)
         menu.exec(a0.globalPos())
 
 
-class WebViewWindow(QWidget):
+class WebViewWindow(QtWidgets.QWidget):
     def __init__(self, parent=None, *args):
         super().__init__(parent, *args)
         self.url = ""
         self.browser = CustomWebEngineView(self)
-        self.browser.load(QUrl(self.url))
         self.browser.resize(1000, 600)
         self.browser.setWindowTitle("Task")
+        self.url_edit = QtWidgets.QLineEdit()
+        self.url_edit.returnPressed.connect(self.loadPage)
 
-        grid = QGridLayout()
+        grid = QtWidgets.QGridLayout()
+        grid.addWidget(self.url_edit, 1, 1, 1, 10)
         grid.addWidget(self.browser, 2, 0, 5, 15)
         self.setLayout(grid)
         self.resize(500, 800)
 
     def download_task(self):
-        self.url = self.browser.url().toString()
+        self.url_edit.setText(self.browser.url().toString())
         self.parent().parent().download(self.url)
 
     def loadPage(self):
-        if self.url:
-            self.browser.load(QUrl(self.url))
+        if self.url_edit.text():
+            self.browser.load(QUrl(self.url_edit.text()))
 
     def changePage(self, url):
-        self.url = url
+        self.url_edit.setText(url)
         self.loadPage()
 
 
 if __name__ == "__main__":
-    app = QApplication(sys.argv)
+    app = QtWidgets.QApplication(sys.argv)
     ex = WebViewWindow()
     sys.exit(app.exec_())
