2e5b8886d7453c47567ed5edb5d9d1e14caa3c4c
fukatani <nannyakannya@gmail.com>
2019-11-08 23:46:09 +0900

    clear if unselected.

--- rujaion/editor.py
+++ rujaion/editor.py
@@ -376,12 +376,13 @@ class Editter(QtWidgets.QPlainTextEdit):
 
     def levenshteinize(self):
         center = self.textCursor().selectedText()
-        if not center:
-            return
-        # words = set(re.findall(r"\b[a-zA-Z_]+[a-zA-Z1-9_]\b", self.toPlainText(), re.S))
-        # words = sort_by_levenshtein(center, words, 1)
-        # self.highlighter.update_levenshtein(words)
-        self.highlighter.update_levenshtein([center,])
+        if center:
+            self.highlighter.update_levenshtein([center, ])
+            # words = set(re.findall(r"\b[a-zA-Z_]+[a-zA-Z1-9_]\b", self.toPlainText(), re.S))
+            # words = sort_by_levenshtein(center, words, 1)
+            # self.highlighter.update_levenshtein(words)
+        else:
+            self.highlighter.levensteign_rules.clear()
         self.highlighter.rehighlight()
 
     def keyPressEvent(self, event: QtGui.QKeyEvent):
