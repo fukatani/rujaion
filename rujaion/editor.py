9ecab29bb626dac9a04912b5e7a055366d5813dc
Unknown <nannyakannya@gmail.com>
2019-11-09 13:44:11 +0900

    black

--- rujaion/editor.py
+++ rujaion/editor.py
@@ -377,7 +377,7 @@ class Editter(QtWidgets.QPlainTextEdit):
     def levenshteinize(self):
         center = self.textCursor().selectedText()
         if center:
-            self.highlighter.update_levenshtein([center, ])
+            self.highlighter.update_levenshtein([center])
             # words = set(re.findall(r"\b[a-zA-Z_]+[a-zA-Z1-9_]\b", self.toPlainText(), re.S))
             # words = sort_by_levenshtein(center, words, 1)
             # self.highlighter.update_levenshtein(words)
