923e8adf5d933c47b8dd8608d9ca6c1c20ea7f09
fukatani <nannyakannya@gmail.com>
2020-04-04 15:27:49 +0900

    Support variable.

--- rujaion/extract.py
+++ rujaion/extract.py
@@ -8,13 +8,24 @@ class ExtractDialog(QtWidgets.QDialog):
         super().__init__(*args)
         self.sentence_edit = util.StateLessTextEdit(sentence, self)
         self.name_edit = util.StateLessTextEdit("", self)
-        self.extract_type = QtWidgets.QRadioButton()
-        self.extract_type.setChecked(True)
+
+        self.extract_kind_box = QtWidgets.QGroupBox("Extract kind")
+        vbox = QtWidgets.QVBoxLayout()
+        self.extract_var = QtWidgets.QRadioButton("As Variable")
+        self.extract_var.setChecked(True)
+        vbox.addWidget(self.extract_var)
+        self.extract_type = QtWidgets.QRadioButton("As Type")
+        vbox.addWidget(self.extract_type)
+        self.extract_kind_box.setLayout(vbox)
+        self.replace_all_checkbox = QtWidgets.QCheckBox()
+        self.replace_all_checkbox.setChecked(True)
+
         self.dialogs = (
             ("Extract...", None),
             ("Sentence", self.sentence_edit),
             ("Name", self.name_edit),
-            ("Type", self.extract_type),
+            ("Kind", self.extract_kind_box),
+            ("Replace All", self.replace_all_checkbox),
         )
         self.resize(500, 100)
         self.draw()
@@ -53,15 +64,19 @@ class ExtractDialog(QtWidgets.QDialog):
         self.setLayout(main_layout)
 
     def extract(self):
+        if self.extract_var.isChecked():
+            declaration = "let {} = {};\n".format(self.name_edit.text(), self.sentence_edit.text())
+
         if self.extract_type.isChecked():
             declaration = "type {} = {};\n".format(self.name_edit.text(), self.sentence_edit.text())
 
+        if self.replace_all_checkbox.isChecked():
             pos = self.parent().save_position()
             text = self.parent().toPlainText()
             self.parent().clear_and_write_text(text.replace(self.sentence_edit.text(), self.name_edit.text()))
             self.parent().load_position(*pos)
 
-            tc = self.parent().textCursor()
-            tc.movePosition(QtGui.QTextCursor.StartOfLine)
-            tc.insertText(declaration)
+        tc = self.parent().textCursor()
+        tc.movePosition(QtGui.QTextCursor.StartOfLine)
+        tc.insertText(declaration)
         self.close()
