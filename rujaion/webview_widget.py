87ed0cfab7c96cd73d83718feca42f0342b36429
fukatani <nannyakannya@gmail.com>
2020-04-04 23:16:45 +0900

    remove alert

--- rujaion/webview_widget.py
+++ rujaion/webview_widget.py
@@ -59,6 +59,9 @@ navi_script = """// ==UserScript==
            var dom_obj_parent = dom_obj.parentNode.parentNode;
            dom_obj_parent.removeChild(dom_obj.parentNode);
         }
+        for (obj of document.getElementsByClassName("alert alert-warning alert-dismissible fade in")) {
+            obj.innerHTML = '';
+        }
 
         const tasksBar = document.createElement('l');
         tasksBar.className = 'nav nav-tabs';
